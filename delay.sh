ff7494cede9f410ed3728891c99239af4f78ab89
Joshua B. <desertflame3@gmail.com>
2024-07-29 15:08:46 -0400

    Update delay.sh

--- delay.sh
+++ delay.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-sleep_duration=${1:-30}
+# Health check handler
+if [ "$1" = "--check-health" ]; then
+  if [ -f /tmp/end_time ]; then
+    [ $(date +%s) -ge $(cat /tmp/end_time) ]
+  else
+    exit 1
+  fi
+  exit
+fi
+
+# Set sleep duration
+sleep_duration=${1:-${DELAY_SECONDS:-30}}
 end_time=$(($(date +%s) + sleep_duration))
+echo $end_time > /tmp/end_time
 
-health_check() {
-  [ $(date +%s) -ge $end_time ]
-}
+# Set up trap for clean exit
+trap 'rm -f /tmp/end_time; exit 0' TERM INT
 
-trap 'exit 0' TERM INT
-
-while ! health_check; do
+# Main delay loop
+while [ $(date +%s) -lt $end_time ]; do
   sleep 1
 done
+
+# Clean up
+rm -f /tmp/end_time
